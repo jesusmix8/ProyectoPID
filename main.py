23826dc1f7b02d992f9f3bf0e7d271d531c27349
Jesus Sanchez <jesusmix8@icloud.com>
2023-10-13 18:44:37 -0600

    alv ya no se jjajsj

--- main.py
+++ main.py
@@ -1,35 +1,154 @@
-def opcion1():
-    # Lógica para la opción 1
-    print("Hola y Adios")
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+import cv2
+import numpy as np
 
-def opcion2():
-    # Lógica para la opción 2
-    print("Hola")
+class ImageProcessingApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Procesamiento de Imágenes")
+        self.root.geometry("1250x650")
+        self.root.minsize(700, 480) # Ancho x Alto
 
-def opcion3():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion4():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion5():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion6():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion7():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion8():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion9():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion10():
-    # Lógica para la opción 3
-    print("Hola")
-def opcion11():
-    # Lógica para la opción 3
-    print("Hola")
+        self.colorbg = "#27374D"
+        self.sidemenubg = "#526D82"
+        self.botonesbg = "#9DB2BF"
+
+        self.create_menu_frame()
+        self.create_content_frame()
+        self.create_buttons()
+        self.create_functionbotones()
+        self.desactivar_botones()
+        self.imagen = None  # Variable para almacenar la imagen
+        self.imagen_procesada = None  # Variable para almacenar la imagen procesada
+
+    def create_menu_frame(self):
+        self.menu_frame = tk.Frame(self.root, width=150, bg=self.sidemenubg)
+        self.menu_frame.pack(side="left", fill="y")
+
+    def create_content_frame(self):
+        self.contenido_frame = tk.Frame(self.root, bg=self.colorbg)
+        self.contenido_frame.pack(expand=True, fill="both")
+        self.label = tk.Label(self.contenido_frame, text="Cargue una imagen para empezar", font=("Montserrat", 25), bg=self.colorbg, fg="White")
+        self.label.pack(expand=True, fill="both")
+        self.image_label = tk.Label(self.contenido_frame)  # Para mostrar la imagen
+        self.image_label.pack()
+
+
+    def create_buttons(self):
+        self.boton_width = 20
+        buttons_data = [
+            ("Ecualización", self.Ecualización),
+            ("Inversion binaria", self.InversionB),
+            ("Inversión fotográfica", self.inversionF),
+            ("Crear collage", self.collage),
+            ("Rotar imagen 45°", self.Rotar),
+            ("Espejo", self.Espejo),
+            ("Filtros", self.Filtros),
+            ("Erosionar", self.Erosionar),
+            ("Dilatar", self.Dilatar),
+            ("Modificar color de ojos", self.ojos),
+            ("Segmentación para \n N renglones", self.Segmentación),
+        ]
+
+        for text, command in buttons_data:
+            button = tk.Button(self.menu_frame, text=text, command=command, width=self.boton_width, font=("Montserrat"), bg=self.botonesbg)
+            button.pack()
+
+    def create_functionbotones(self):
+        self.botonLoadImage = tk.Button(self.contenido_frame, text="Cargar imagen", width=20, height=5, font=("Montserrat"), bg=self.colorbg, fg="White", command=self.cargar_imagen)
+        self.botonLoadImage.pack(pady=75)
+        botonSalir = tk.Button(self.menu_frame, text="Salir", command=self.root.destroy, width=self.boton_width, bg="#0F2C59", fg="White", font=("Montserrat"))
+        botonSalir.config(state="normal")
+        botonSalir.pack(side="bottom" )
+
+    def cargar_imagen(self):
+        self.rutadeArchivo = filedialog.askopenfilename(title="Seleccione una imagen")
+        if self.rutadeArchivo:
+            self.imagen = Image.open(self.rutadeArchivo)
+            self.mostrar_imagen(self.imagen)
+            self.imagencv = cv2.imread(self.rutadeArchivo)
+        self.botonLoadImage.destroy()
+        self.label.destroy()
+        self.activar_botones()    
+
+    def desactivar_botones(self):
+        for widget in self.menu_frame.winfo_children():
+            if isinstance(widget, tk.Button) and widget.cget("text") != "Salir":
+                widget.config(state="disabled")
+
+    def activar_botones(self):
+        for widget in self.menu_frame.winfo_children():
+            if isinstance(widget, tk.Button):
+                widget.config(state="normal")
+
+    def mostrar_imagen(self, imagen):
+        if imagen:
+            photo = ImageTk.PhotoImage(imagen)
+            self.image_label.config(image=photo)
+            self.image_label.image = photo
+            self.image_label.pack(expand=True, anchor="center")
+        self.label.destroy()
+
+    def Ecualización(self):
+        image = cv2.imread(self.rutadeArchivo, cv2.IMREAD_GRAYSCALE)
+        imagenEcualizada = cv2.equalizeHist(image)
+        self.imagen_procesada = Image.fromarray(imagenEcualizada)
+        self.mostrar_imagen(self.imagen_procesada)
+
+    def InversionB(self):
+        image = cv2.imread(self.rutadeArchivo,cv2.IMREAD_GRAYSCALE)
+        imageninvertida = 255 - image
+        self.imagen_procesada = Image.fromarray(imageninvertida)
+        self.mostrar_imagen(self.imagen_procesada)
+        pass
+
+    def inversionF(self):
+        # Implementa la lógica para la opción 3
+        pass
+
+    def collage(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+    def Rotar(self):
+        image = Image.open(self.rutadeArchivo)
+        imagenRotada = image.rotate(45)
+        self.imagen_procesada = imagenRotada
+        self.mostrar_imagen(self.imagen_procesada)
+        
+
+        
+
+    def Espejo(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+    def Filtros(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+
+    def Erosionar(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+    def Dilatar(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+    def ojos(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+    def Segmentación(self):
+        # Implementa la lógica para la opción 1
+        pass
+
+    # Definir funciones opcion2, opcion3, etc.
+
+if __name__ == "__main__":
+    ventana = tk.Tk()
+    app = ImageProcessingApp(ventana)
+    ventana.mainloop()
